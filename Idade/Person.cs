3f2661b8b2e79580b9dd136a7b70a422eba0b65b
João Gabriel <joaogabrielffaria179@gmail.com>
2024-05-20 18:43:25 -0300

    Atividade Individual Caso de Exceção

---
+++ Idade/Person.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Idade
+{
+    public class Person
+    {
+        //variaveis
+        private string firstname;
+        private int age;
+
+        //construtor
+        public Person(string firstName, int age)
+        {
+            this.Nome1 = firstName;
+            this.Age = age;
+        }
+
+        //get set
+        public string Nome1
+        {
+            get { return firstname; }
+            set { firstname = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        //metodos
+        public void imprimir()
+        {
+            Console.WriteLine("Nome: " + this.Nome1);
+            Console.WriteLine("Idade: " + this.Age);
+            
+        }
+        public void checarIdade(int Age)
+        {
+            {
+                if (Age < 18)
+               {
+                    throw new ArithmeticException("Acesso negado - Você não é maior de 18.");
+                }
+                else
+                {
+                    Console.WriteLine("Acesso permitido - Você é maior de 18");
+                }
+            }
+        }
+    }
+
+}
---
+++ Idade/Program.cs
@@ -0,0 +1,31 @@
+﻿// crie uma classe Pessoa para armazenar o Nome e Idade do usuario.
+// A classe também deve disponibilizar os metodos:
+// "checarIdade" armazena a idade indicada pelo usuario e usa caso de exceção para checar se o usuario é maior de 18 anos.
+// Permitindo o acesso se for maior de 18 anos, ou negando o acesso se for menor de 18.
+// "imprimir" Imprime as informações do usuario (nome e idade).
+
+using Idade;
+using System;
+class MainClass {
+    static void Main(string[]args) {
+
+        string Nome1;
+        int Age;
+
+        Console.WriteLine("Qual o seu nome?");
+        Nome1 = Console.ReadLine();
+        Console.WriteLine("Qual a sua idade?");
+        Age = Convert.ToInt32(Console.ReadLine());
+
+        Person pessoa = new Person(Nome1, Age);
+        
+
+        pessoa.imprimir();
+        pessoa.checarIdade(Age);
+    }
+}
+
+
+
+
+
